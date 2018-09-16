73eb47b7f773744308205e5a43c99c1849950f07
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-16 18:08:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do {|win_combination|}
+  WIN_COMBINATIONS.each do {|win_combination|
     win_index_1 = win_combination[0]  # grabs each index from the win_combination
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -26,11 +26,9 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return WIN_COMBINATIONS[0]
-    else 
-      false
-    end
-  end
-
-
-
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+        end
+      }
+      return false
