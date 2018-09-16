4c315ef85b42d0280ba88eddb0b9a9428711406c
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-16 18:27:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,19 +17,9 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-   win_index_1 = win_combination[0]  # grabs each index from the win_combination
-   win_index_2 = win_combination[1]
-   win_index_3 = win_combination[2]
-# loads values of board at winning indexes
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
-        end
-      return false
-    end
+    return win_combination if win_combination.all? {|position| board[position] == "X"}
+    return win_combination if win_combination.all? {|position| board[position] == "O"}
+  end
+    return false if board.all? {|position| position != " "}
+    return false if board.all? {|position| position == " "}
   end
